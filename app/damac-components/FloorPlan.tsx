9354ac78c547e75400195a66923987bf21255bea
Sailesh <drevendor1@gmail.com>
2026-02-20 14:55:40 +0400

    Upload landing pages NEW

--- app/damac-components/FloorPlan.tsx
+++ app/damac-components/FloorPlan.tsx
@@ -104,6 +104,7 @@ const FloorPlan: React.FC = () => {
                          onClose={closeEnquiryModal}
                          floorPlanTitle="DAMAC ISLANDS 2"
                          buttonText={buttonText}
+                         projectTitle="Damac Islands 2 | Premium Waterfront Townhouses and Villas"
                        />
                        </div>
       </div>
